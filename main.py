9933c95f69b915547bd256389a0374fdf758c9a7
Tyler <conlontj@clarkson.edu>
2020-04-14 18:44:05 -0400

    april 14 class

--- main.py
+++ main.py
@@ -17,8 +17,37 @@ def send_static(path):
   
 @app.route('/')
 def home():
-    return render_template('test.html', title='Home', msg='Welcome!')
+    #return '<b>hi</b>'
+    return render_template('test.html', title='Home', msg='Welcome!!!!')
 
+@app.route('/countStates')
+def countStates():
+    state = request.args.get('state')
+    fn = 'subsetA_parking_violations_2016.csv'
+    f = open(fn,'r')
+    reader = csv.reader(f)
+    statehist = {}
+    n=0
+    for row in reader:
+        if n > 0:
+            if (state is not None and state == row[2]) or state is None:
+                k = row[2]
+                if k in statehist.keys():
+                    statehist[k] += 1
+                else:
+                    statehist[k] = 1
+            
+        n+=1
+    f.close()
+    sorted_states = sorted(statehist.items(),key=operator.itemgetter(1),reverse=True)
+    print(sorted_states)
+    #print(statehist)
+    #return json.dumps(statehist)   
+    s = ''
+    for st in sorted_states:
+        s += st[0] + ' ' + str(st[1]) + '<br>'
+    return render_template('states.html', title='State count', states=sorted_states)
+    
 @app.route('/csv')  
 def download_csv():  
     csv = 'a,b,c\n1,2,3\n'  
@@ -28,6 +57,7 @@ def download_csv():
     response.mimetype='text/csv'
 
     return response
+
 
     
 if __name__ == "__main__":
