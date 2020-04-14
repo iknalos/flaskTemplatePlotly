0f24139ea5c871f71f641d86af1b4a21f88143da
Tyler <conlontj@clarkson.edu>
2020-04-14 17:17:50 -0400

    changed to templates

--- main.py
+++ main.py
@@ -15,87 +15,9 @@ app = Flask(__name__, static_url_path='')
 def send_static(path):
     return send_from_directory('static', path)
   
-@app.route("/process", methods=['GET','POST'])
-def process():
-    #request.args.get('action')
-    print request.form.get('sd') 
-    return 'email:'+ request.form.get('email') 
-
-@app.route("/countByState", methods=['GET','POST'])
-def countByState():   
-    with open('subsetA_parking_violations_2016.csv') as f:
-        data = [{k: str(v) for k, v in row.items()}
-            for row in csv.DictReader(f, skipinitialspace=True)]
-
-    sc ={}
-    for row in data:
-        st = row['Registration State']
-        if st in sc.keys():
-            sc[st] += 1
-        else:
-            sc[st] = 1
-    
-
-    sorted_sc = sorted(sc.items(),key=operator.itemgetter(1),reverse=True)
-    print sorted_sc
-    html = '''
-    <table>
-        <tr style="background-color:#eee;">
-            <td>State</td>
-            <td>Count</td>
-        </tr>'''
-        
-    for row in sorted_sc:
-        html += '''<tr>
-            <td>'''+row[0]+'''</td>
-            <td>'''+str(row[1])+'''</td>
-        </tr>'''
-        
-    html += '''</table> '''
-    return html
-@app.route("/countByStateCSV", methods=['GET','POST'])
-def countByStateCSV():   
-    with open('subsetA_parking_violations_2016.csv') as f:
-        data = [{k: str(v) for k, v in row.items()}
-            for row in csv.DictReader(f, skipinitialspace=True)]
-
-    sc ={}
-    for row in data:
-        st = row['Registration State']
-        if st in sc.keys():
-            sc[st] += 1
-        else:
-            sc[st] = 1
-    
-
-    sorted_sc = sorted(sc.items(),key=operator.itemgetter(1),reverse=True)
-    print sorted_sc
-    buf = 'State,Count\n'
-        
-    for row in sorted_sc:
-        buf +=row[0]+','+str(row[1])+'\n'
-        
-    response = make_response(buf)
-    cd = 'attachment; filename=mycsv.csv'
-    response.headers['Content-Disposition'] = cd 
-    response.mimetype='text/csv'
-
-    return response   
-@app.route("/", methods=['GET','POST'])
-def data():
-    return '''
-    <form id="myform" action="/process" method="POST">
-            Enter string<br>
-            <input type="text" name="email" value="123"/>
-            <br><br>
-            <select name="choice">
-                <option value="yes">yes</option>
-                <option value="no">no</option>
-                <option value="not sure">not sure</option>
-            </select>
-            <input type="submit" value="Submit"/>
-        </form>'''
-    
+@app.route('/')
+def home():
+    return render_template('test.html', title='Home', msg='Welcome!')
 
 @app.route('/csv')  
 def download_csv():  
