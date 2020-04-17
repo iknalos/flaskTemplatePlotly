9632ce9477aa99eed8499641c43d95d05e4f7f32
Tyler <conlontj@clarkson.edu>
2020-04-16 20:51:33 -0400

    april 16

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ app = Flask(__name__, static_url_path='')
 @app.route('/static/<path:path>')
 def send_static(path):
     return send_from_directory('static', path)
-  
+
+
 @app.route('/')
 def home():
     #return '<b>hi</b>'
@@ -40,14 +41,37 @@ def countStates():
         n+=1
     f.close()
     sorted_states = sorted(statehist.items(),key=operator.itemgetter(1),reverse=True)
-    print(sorted_states)
+    #print(sorted_states)
     #print(statehist)
     #return json.dumps(statehist)   
     s = ''
     for st in sorted_states:
         s += st[0] + ' ' + str(st[1]) + '<br>'
     return render_template('states.html', title='State count', states=sorted_states)
-    
+
+@app.route('/countStatesPlot')
+def countStatesPlot():
+    fn = 'subsetA_parking_violations_2016.csv'
+    f = open(fn,'r')
+    reader = csv.reader(f)
+    statehist = {}
+    n=0
+    for row in reader:
+        if n > 0:
+            k = row[2]
+            if k in statehist.keys():
+                statehist[k] += 1
+            else:
+                statehist[k] = 1
+            
+        n+=1
+    f.close()
+    sorted_states = sorted(statehist.items(),key=operator.itemgetter(1),reverse=True)
+    #print(sorted_states)
+    #print(statehist)
+    #return json.dumps(sorted_states)   
+
+    return render_template('statesPlot.html', title='State Plot', states=sorted_states)  
 @app.route('/csv')  
 def download_csv():  
     csv = 'a,b,c\n1,2,3\n'  
